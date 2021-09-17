31cd840760acffbd0a1329398a6936a5ec011094
lucacarotenuto <luca.carotenuto@outlook.de>
2021-09-17 17:06:11 +0200

    postproc and visualize preds

---
+++ scripts/postproc.py
@@ -0,0 +1,35 @@
+# process prediction and visualize
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+pred = np.load('/Users/carotenuto/Documents/GitHub/3d-facial-landmarks-omfs/MeshCNN/datasets/headspace_cust1/predictions.pkl')
+pred = np.reshape(pred, (68,3))
+print(pred.shape)
+
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = pred[:,0]
+y = pred[:,1]
+z = pred[:,2]
+
+
+
+ax.scatter(x, y, z, c='r', marker='o')
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+# rotate the axes and update
+for angle in range(0, 360):
+    ax.view_init(30, angle)
+    plt.draw()
+    plt.pause(.001)
